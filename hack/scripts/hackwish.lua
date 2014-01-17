73d84316a35c05f2ed06baf294de8d5bad585a4a
Putnam3145 <bpwhite@cableone.net>
2014-01-16 16:44:16 -0800

    Updated the Ultimate Reward.

--- hack/scripts/hackwish.lua
+++ hack/scripts/hackwish.lua
@@ -45,15 +45,15 @@ end
 function hackWish(posOrUnit)
     local pos = df.unit:is_instance(posOrUnit) and posOrUnit.pos or posOrUnit
     script.start(function()
---        local amountok, amount
+        local amountok, amount
         local itemok,itemtype,itemsubtype=script.showItemPrompt('What item do you want?',function() return true end,true)
         local matok,mattype,matindex=script.showMaterialPrompt('Wish','And what material should it be made of?')
         local qualityok,quality=script.showListPrompt('Wish','What quality should it be?',COLOR_LIGHTGREEN,qualityTable())
---        repeat amountok,amount=script.showInputPrompt('Wish','How many do you want? (numbers only!)',COLOR_LIGHTGREEN) until tonumber(amount)
+        repeat amountok,amount=script.showInputPrompt('Wish','How many do you want? (numbers only!)',COLOR_LIGHTGREEN) until tonumber(amount)
         if mattype and itemtype then
---            for i=1,tonumber(amount) do
-            createItem({mattype,matindex},{itemtype,itemsubtype},quality,pos)
---            end
+            for i=1,tonumber(amount) do
+                createItem({mattype,matindex},{itemtype,itemsubtype},quality,pos)
+            end
         end
     end)
 end
