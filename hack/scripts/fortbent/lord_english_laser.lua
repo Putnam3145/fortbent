cb0a19e2b28b0ff7be6e95931e71080f58d822df
Putnam3145 <bpwhite@cableone.net>
2014-02-19 10:07:03 -0800

    Made script not terrible.

--- hack/scripts/fortbent/lord_english_laser.lua
+++ hack/scripts/fortbent/lord_english_laser.lua
@@ -3,9 +3,4 @@ local function killUnit(unit)
 	unit.animal.vanish_countdown = 2 --Even if they're undead, the bastard can still double-kill.
 end
 
-local function takeArgAndShoveIt(arg) --shove it into the unit kill function, of course. What else could it mean. Seriously.
-	local unit = df.global.world.units.active[arg]
-	killUnit(unit)
-end
-
-if ... then takeArgAndShoveIt(tonumber(...)) end
+killUnit(df.unit.find(...))
