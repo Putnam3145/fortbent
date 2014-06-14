1414bffaeabf876dde0b056fc8ba8e487282cbac
Putnam3145 <bpwhite@cableone.net>
2014-06-13 19:20:28 -0700

    Cut out chaff for 2014 version and updated creatures and entities for DF2014.

--- hack/scripts/fortbent/maid_of_doom_destroy_undead.lua
+++
@@ -1,7 +0,0 @@
-local function destroyUndead(unit)
-	if not unit.status.current_soul then
-		unit.animal.vanish_countdown = 2
-	end
-end
-
-destroyUndead(df.unit.find(...))
--- hack/scripts/fortbent/witch_of_heart_deattribute.lua
+++
@@ -1,7 +0,0 @@
-local function deattribute(unit)
-    for _,attribute in ipairs(unit.status.current_soul.mental_attrs) do
-		attribute.value = attribute.value/2
-	end
-end
-
-deattribute(df.unit.find(...))
