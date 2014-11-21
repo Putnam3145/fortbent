2bd084597ac930d1674a71e8e3079e37afb8ff40
Putnam3145 <bpwhite@cableone.net>
2014-11-20 18:42:47 -0800

    Fixed ectobiology, reworked build grist
    Build Grist can now be gotten by alchemizing logs and boulders.

--- hack/scripts/fortbent/ectobiology.lua
+++ hack/scripts/fortbent/ectobiology.lua
@@ -8,16 +8,17 @@ local function getCitizenList(lovers_only)
     local citizenTable={}
     if lovers_only then
         for k,u in ipairs(df.global.world.units.active) do
-            if dfhack.units.isCitizen(u) and (unit.relations.spouse_id~=-1 or unit.relations.lover_id~=-1) then
-                table.insert(citizenTable,{dfhack.TranslateName(dfhack.getVisibleName(u)),nil,u})
+            if dfhack.units.isCitizen(u) and (u.relations.spouse_id~=-1 or u.relations.lover_id~=-1) then
+                table.insert(citizenTable,{dfhack.TranslateName(dfhack.units.getVisibleName(u)),nil,u})
             end
         end
     else
         for k,u in ipairs(df.global.world.units.active) do
             if dfhack.units.isCitizen(u) then
-                table.insert(citizenTable,{dfhack.TranslateName(dfhack.getVisibleName(u)),nil,u})
+                table.insert(citizenTable,{dfhack.TranslateName(dfhack.units.getVisibleName(u)),nil,u})
             end
         end
+    end
     return citizenTable
 end
 
@@ -46,43 +47,42 @@ local function ectobiologize(freeform)
     local script=require('gui.script')
     script.start(function()
     local citizens=getCitizenList(not freeform)
+    if #citizens==0 then script.showMessage('Ectobiology',"Nobody is in a relationship! Best use freeform ectobiology.",COLOR_WHITE) return end
     if freeform then
-        local ok1,name1,unit1=script.showListPrompt("Unit Selection","Choose first paradox ghost slime target.",COLOR_WHITE,citizens)
-        local ok2,name2,unit2=script.showListPrompt("Unit Selection","Choose second paradox ghost slime target.",COLOR_WHITE,citizens)
+        local ok1,name1,unit1_t=script.showListPrompt("Ectobiology","Choose first paradox ghost slime target.",COLOR_WHITE,citizens)
+        local ok2,name2,unit2_t=script.showListPrompt("Ectobiology","Choose second paradox ghost slime target.",COLOR_WHITE,citizens)
+        local unit1=unit1_t[3]
+        local unit2=unit2_t[3]
         unit1.relations.pregnancy_timer=1
-        unit1.relations.pregnancy_genes=unit.appearance.genes:new()
+        unit1.relations.pregnancy_genes=unit1.appearance.genes:new()
         unit1.relations.pregnancy_spouse=unit2.hist_figure_id
         unit1.relations.pregnancy_caste=unit2.caste
-        dfhack.run_script('modtools/add-syndrome','-syndrome','temp desterilize','-target',unit.id)
+        dfhack.run_script('modtools/add-syndrome','-syndrome','temp desterilize','-target',unit1.id)
         if unit1.sex==1 then
+            local normal_caste=unit1.enemy.normal_caste
+            unit1.enemy.normal_caste=getFemaleCasteWithSameMaxAge(unit1)
+            script.sleep(1,'ticks')
+            unit1.enemy.normal_caste=normal_caste
+        end
+    else
+        local ok,name,unit_t=script.showListPrompt("Ectobiology","Choose first genetic material giver.",COLOR_WHITE,citizens)
+        local unit=unit_t[3]
+        local lover=getSpouseOrLover(unit)
+        unit.relations.pregnancy_timer=1
+        unit.relations.pregnancy_genes=unit.appearance.genes:new()
+        unit.relations.pregnancy_spouse=lover
+        unit.relations.pregnancy_caste=df.historical_figure.find(lover).caste
+        dfhack.run_script('modtools/add-syndrome','-syndrome','temp desterilize','-target',unit.id)
+        if unit.sex==1 then
             local normal_caste=unit.enemy.normal_caste
             unit.enemy.normal_caste=getFemaleCasteWithSameMaxAge(unit)
             script.sleep(1,'ticks')
             unit.enemy.normal_caste=normal_caste
         end
-    else
-        local ok,name,unit=script.showListPrompt("Unit Selection","Choose first genetic material giver.",COLOR_WHITE,citizens)
-        if unit.relations.spouse_id==-1 and unit.relations.lover_id==-1 then
-            script.showMessage('Unit selection',"That person ain't in a relationship, you ninnywhipper! Use freeform ectobiology for your weird shipping shit.")
-        else
-            local lover=getSpouseOrLover(unit)
-            unit.relations.pregnancy_timer=1
-            unit.relations.pregnancy_genes=unit.appearance.genes:new()
-            unit.relations.pregnancy_spouse=lover
-            unit.relations.pregnancy_caste=df.historical_figure.find(lover).caste
-            dfhack.run_script('modtools/add-syndrome','-syndrome','temp desterilize','-target',unit.id)
-            if unit.sex==1 then
-                local normal_caste=unit.enemy.normal_caste
-                unit.enemy.normal_caste=getFemaleCasteWithSameMaxAge(unit)
-                script.sleep(1,'ticks')
-                unit.enemy.normal_caste=normal_caste
-            end
-        end
     end
     end)
 end
-
-local utils=require(utils)
+local utils=require('utils')
 
 validArgs = validArgs or utils.invert({
  'freeform'
