3b8d29c88e24a93e92ecb4aa43e1179b3be5363f
Putnam3145 <bpwhite@cableone.net>
2014-11-28 00:22:37 -0800

    Added flashing colors to mind blasts.
    Why am I doing this.

--- raw/init.lua
+++ raw/init.lua
@@ -1,13 +1,22 @@
-local colors={COLOR_LIGHTRED,COLOR_YELLOW,COLOR_BLUE,COLOR_GREEN,COLOR_MAGENTA,COLOR_BROWN}
+local majyyk_colors={COLOR_LIGHTRED,COLOR_YELLOW,COLOR_BLUE,COLOR_GREEN,COLOR_MAGENTA,COLOR_BROWN}
 local clockwork_majyyks=dfhack.matinfo.find('CLOCKWORK_MAJYYKS').material
 local clockwork_cur_color_idx=0
 clockwork_majyyks.build_color[2]=0
 clockwork_majyyks.tile_color[2]=0
+local psiioniic_colors={COLOR_LIGHTRED,COLOR_LIGHTBLUE}
+local optic_blast=dfhack.matinfo.find('MINDBLAST').material
+local optic_blast_cur_color_idx=0
+optic_blast.build_color[2]=0
+optic_blast.tile_color[2]=0
+optic_blast.build_color[1]=0
+optic_blast.tile_color[1]=0
+optic_blast.basic_color[1]=0
+
 
 local function clockwork_majyyks_color_change()
     clockwork_cur_color_idx=(clockwork_cur_color_idx%6)+1
-    clockwork_cur_color_f=colors[clockwork_cur_color_idx]
-    clockwork_cur_color_b=colors[(clockwork_cur_color_idx%6)+1]
+    local clockwork_cur_color_f=majyyk_colors[clockwork_cur_color_idx]
+    local clockwork_cur_color_b=majyyk_colors[(clockwork_cur_color_idx%6)+1]
     clockwork_majyyks.basic_color[0]=clockwork_cur_color_f
     clockwork_majyyks.basic_color[1]=clockwork_cur_color_b
     clockwork_majyyks.build_color[0]=clockwork_cur_color_f
@@ -16,9 +25,19 @@ local function clockwork_majyyks_color_change()
     clockwork_majyyks.tile_color[1]=clockwork_cur_color_b
 end
 
+local function mind_blast_color_change()
+    optic_blast_cur_color_idx=(optic_blast_cur_color_idx%2)+1
+    local optic_color=psiioniic_colors[optic_blast_cur_color_idx]
+    clockwork_majyyks.basic_color[0]=optic_color
+    clockwork_majyyks.build_color[0]=optic_color
+    clockwork_majyyks.tile_color[0]=optic_color    
+end
+
 local repeat_util=require('repeat-util')
 
 repeat_util.scheduleUnlessAlreadyScheduled('Clockwork Majyyks',math.ceil(df.global.enabler.gfps/20),'frames',clockwork_majyyks_color_change)
+
+repeat_util.scheduleUnlessAlreadyScheduled('Psiioniic Blast',5,'ticks',mind_blast_color_change)
 
 if not pcall(function() require('plugins.dfusion.friendship') end) then qerror("Friendship couldn't be installed! God tiers will be wonkier than usual.") end
 
