e7e279db4247256e25fd2a53890b323743b84188
Putnam3145 <bpwhite@cableone.net>
2017-12-09 23:39:53 -0800

    yeah i just couldn't handle seeing whatever that was

--- raw/scripts/fortbent/witch_of_heart_deattribute.lua
+++ raw/scripts/fortbent/witch_of_heart_deattribute.lua
@@ -1,8 +1,8 @@
 local function deattribute(unit)
     local rng=dfhack.random.new()
     for _,attribute in ipairs(unit.status.current_soul.mental_attrs) do
-		attribute.value = math.ceil(attribute.value*((rng:drandom1()+1)/2))
-	end
+		  attribute.value = math.ceil(attribute.value*((rng:drandom1()+1)/2))
+	  end
 end
 
 deattribute(df.unit.find(...))
