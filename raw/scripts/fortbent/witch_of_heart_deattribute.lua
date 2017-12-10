815ec1eaffb79b18f107cf579d2b976c97430494
Putnam3145 <bpwhite@cableone.net>
2017-12-09 23:40:17 -0800

    cursed file

--- raw/scripts/fortbent/witch_of_heart_deattribute.lua
+++ raw/scripts/fortbent/witch_of_heart_deattribute.lua
@@ -1,7 +1,7 @@
 local function deattribute(unit)
     local rng=dfhack.random.new()
     for _,attribute in ipairs(unit.status.current_soul.mental_attrs) do
-		  attribute.value = math.ceil(attribute.value*((rng:drandom1()+1)/2))
+		    attribute.value = math.ceil(attribute.value*((rng:drandom1()+1)/2))
 	  end
 end
 
